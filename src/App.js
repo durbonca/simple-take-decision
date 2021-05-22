5b6bd94c03c0e7cdd011fb5b557614b19c448d68
Manuel Duran <durbonca@gmail.com>
2021-05-22 10:33:13 -0400

    logic created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import {useState} from 'react';
 
 import './App.css';
 
 function App() {
+
+  const [elementsToRaffle, setElementsToRaffle] = useState([])
+  const [element, setElement] = useState('')
+
+  function handleSubmit (event) {
+      event.preventDefault();
+      setElementsToRaffle([...elementsToRaffle, element]) 
+      setElement('')
+  }
+
+  function handleRandom () {
+      let randomDecition = elementsToRaffle[Math.floor(Math.random() * elementsToRaffle.length)]
+      window.alert('LA DAMA HA DICHO!: ' + randomDecition)
+  }
+
   return (
-      <div className="bg-gray-500">hello</div>
+    <div className="bg-gray-300 h-screen flex flex-col">
+      <h1>La Dama de la fortuna</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label ></label>
+        <input 
+          
+          type="text" 
+          value={element} 
+          onChange={(event) => setElement(event.target.value)} />
+        <input type="submit" value="agregar desicion" disabled={!element}/>
+      </form>
+      <ul>
+        { elementsToRaffle.map((item, index)=> <li key={index} > {item} </li> )}
+      </ul>
+
+      <button onClick={()=>handleRandom()} disabled={!elementsToRaffle.length}>SORTEAR A LA DAMA DE LA FORTUNA!</button>
+    </div>
   );
 }
 
